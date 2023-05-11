2f26a684df5b59d8bae3199e151103d12ddba0e0
LuisPulenta <luisalbertonu@gmail.com>
2023-05-11 12:18:25 -0300

    Video 330

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,9 @@ void main() async {
   ], child: const MyApp()));
 }
 
+//---------------------------------------------------------------
+//------------------------ MyApp --------------------------------
+//---------------------------------------------------------------
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -28,6 +31,71 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       theme: AppTheme().getTheme(),
+      builder: (context, child) =>
+          HandleNotificationInteraccions(child: child!),
     );
+  }
+}
+
+//---------------------------------------------------------------
+//--------------- HandleNotificationInteraccions ----------------
+//---------------------------------------------------------------
+class HandleNotificationInteraccions extends StatefulWidget {
+  final Widget child;
+  const HandleNotificationInteraccions({super.key, required this.child});
+
+  @override
+  State<HandleNotificationInteraccions> createState() =>
+      _HandleNotificationInteraccionsState();
+}
+
+class _HandleNotificationInteraccionsState
+    extends State<HandleNotificationInteraccions> {
+  // It is assumed that all messages contain a data field with the key 'type'
+  Future<void> setupInteractedMessage() async {
+    // Get any messages which caused the application to open from
+    // a terminated state.
+    RemoteMessage? initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+
+    // If the message also contains a data property with a "type" of "chat",
+    // navigate to a chat screen
+    if (initialMessage != null) {
+      _handleMessage(initialMessage);
+    }
+
+    // Also handle any interaction when the app is in the background via a
+    // Stream listener
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  }
+
+  void _handleMessage(RemoteMessage message) {
+    context.read<NotificationsBloc>().handleRemoteMessage(message);
+
+    final messageId =
+        message.messageId?.replaceAll(':', '').replaceAll('%', '');
+    appRouter.push('/push-details/$messageId');
+
+    // if (message.data['type'] == 'chat') {
+    //   Navigator.pushNamed(
+    //     context,
+    //     '/chat',
+    //     arguments: ChatArguments(message),
+    //   );
+    // }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Run code required to handle interacted messages in an async function
+    // as initState() must not be async
+    setupInteractedMessage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
   }
 }
